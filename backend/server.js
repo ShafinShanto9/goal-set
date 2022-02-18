e41c19096c8819fb703657103438f6d39ecf524a
shafin_shant0 <shafinshanto9@gmail.com>
2022-02-18 23:46:01 +0600

    set middleware

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
+
+const {errorHandler} = require('./middleware/errorMiddleWare')
                
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended : false}))
+app.use(errorHandler)
 
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.listen(port, () => console.log(`Server started on port ${port}`))
