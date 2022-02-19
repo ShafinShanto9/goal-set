bd83622255fb897e21589bca8c8eb7cfcd188c25
shafin_shant0 <shafinshanto9@gmail.com>
2022-02-20 00:17:30 +0600

    reg-log using jwt

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async() =>{
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`database conected ${conn.connection.host}`.cyan.underline)
+        console.log(`database conected ${conn.connection.host}`.yellow.underline)
     } catch (error) {
         console.log(error)
         process.exit(1)
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,76 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
 
 
-const registerUser = (req, res) => {
-    res.json({message: "register User"})
-}
-const loginUser = (req, res) => {
-    res.json({message: "Login User"})
-}
-const getMe = (req, res) => {
+const registerUser =asyncHandler(async(req, res) => {
+    const {name, email, password} = req.body
+
+    if(!name || !email || !password){
+        res.status(400)
+        throw new Error("Please Add All Fields")
+    }
+
+    // checked is user existeed
+    const userExisted = await User.findOne({email})
+    if(userExisted){
+        res.status(400)
+       throw new Error("User Already Existed")
+    }
+
+    // Hashing Password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // create user
+    const user = await User.create({
+        name,
+        email,
+        password : hashedPassword
+    })
+
+    if(user) {
+        res.status(201).json({
+            _id : user.id,
+            name: user.name,
+            email: user.email,
+            token: genaretToken(user._id)
+        })
+    }else{
+        res.status(400)
+        throw new Error("Invalid User")
+    }
+})
+
+const loginUser = asyncHandler(async(req, res) => {
+    const {email, password} = req.body
+
+    // Checked User
+    const user = await User.findOne({email})
+    if(user && (await bcrypt.compare(password, user.password))){
+        res.json({
+            _id : user.id,
+            name: user.name,
+            email: user.email,
+            token: genaretToken(user._id)
+        })
+    }else{
+        res.status(400)
+        throw new Error("Invalid User")
+    }
+})
+const getMe = asyncHandler(async(req, res) => {
     res.json({message: "User Data"})
+})
+
+// Genaret JWT Token
+
+const genaretToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    })
+
 }
 
 module.exports = {
