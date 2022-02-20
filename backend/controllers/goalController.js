61b8be2e23fb9e7fa06eabfff77b63a193859acf
shafin_shant0 <shafinshanto9@gmail.com>
2022-02-20 13:58:52 +0600

    comple Api

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,8 +1,9 @@
 const asyncHandler = require('express-async-handler')
 const Goal = require('../models/goalModel')
+const User = require('../models/userModel')
  
 const getGoals = asyncHandler(async(req, res) =>{
-    const goals = await Goal.find()
+    const goals = await Goal.find({user: req.user.id})
     res.status(200).json(goals)
 })
 
@@ -12,17 +13,34 @@ const setGoals = asyncHandler( async(req, res) =>{
         throw new Error('Please add a text field')
     }
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        user: req.user.id
     })
     res.status(200).json(goal)
 })
 
 const updateGoals = asyncHandler( async(req, res) =>{
-    const goal = await Goal.findById(req.params.id)
+ const goal = await Goal.findById(req.params.id)
+
     if(!goal){
         res.send(400)
         throw new Error("Goal Not Found")
     }
+
+    const user = await User.findById(req.user.id)
+
+    //checked user
+    if(!user){
+        res.status(401)
+        throw new Error('User not Found')
+    }
+    // make sure user matched
+
+    if(goal.user.toString() !== user.id){
+        res.status(401)
+        throw new Error("user unAthorized")
+    }
+
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
     res.status(200).json(updatedGoal)
 })
@@ -31,6 +49,20 @@ const deleteGoals = asyncHandler( async(req, res) =>{
     if(!goal) {
         res.send(400)
         throw new Error("Goal Not Found")
+    }
+
+    const user = await User.findById(req.user.id)
+
+    //checked user
+    if(!user){
+        res.status(401)
+        throw new Error('User not Found')
+    }
+    // make sure user matched
+
+    if(goal.user.toString() !== user.id){
+        res.status(401)
+        throw new Error("user unAthorized")
     }
 
    await goal.remove()
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -61,7 +61,12 @@ const loginUser = asyncHandler(async(req, res) => {
     }
 })
 const getMe = asyncHandler(async(req, res) => {
-    res.json({message: "User Data"})
+    const {_id, name, email} = await User.findById(req.user.id)
+    res.status(400).json({
+        id : _id,
+        name,
+        email
+    })
 })
 
 // Genaret JWT Token
